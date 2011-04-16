4028acd3492b7d13b86ff3ec8b6c592572764d1d
Pedro Fragoso <ember@pfragoso.org>
2011-04-16 04:03:41 +0100

    Port to gi

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
-#!/usr/bin/env python
-import gobject
-import gtk
-import gio
-import appindicator
-import pynotify
+#!/usr/bin/python
+from gi.repository import Gtk
+from gi.repository import Gio
+from gi.repository import AppIndicator
+from gi.repository import Notify
+
 import os
 
-#YUCK: gtk.image_new_from_gicon does not work, and pynotify does
+#YUCK: Gtk.image_new_from_gicon does not work, and pynotify does
 #not support gicon, so just let the theme choose the icon
 def _get_icon_name_from_gicon(gicon):
-    assert type(gicon) == gio.ThemedIcon
+    assert type(gicon) == Gio.ThemedIcon
     name = "image-missing"
-    theme = gtk.icon_theme_get_default()
+    theme = Gtk.IconTheme.get_default()
     for n in gicon.get_names():
-        if theme.lookup_icon(n, gtk.icon_size_lookup(gtk.ICON_SIZE_MENU)[0], 0):
+        if theme.lookup_icon(n, Gtk.icon_size_lookup(Gtk.IconSize.MENU, 0, 0), 0):
             name = n
             break
     return n
 
+    
 class Monitor:
 
     mon = None
@@ -30,7 +31,7 @@ class Monitor:
         self.main = main
 
     def init(self):
-        self.mon = gio.VolumeMonitor()
+        self.mon = Gio.VolumeMonitor.get()
         self.mon.connect('mount-added', self._add_drive)
         self.mon.connect('mount-removed', self._del_drive)
         self.refresh()
@@ -52,27 +53,30 @@ class Monitor:
         for m in self.mon.get_mounts():
             self._add_drive(None, m)
 
-    def _eject_cb(self, m, result):
+    def _eject_cb(self, m, result, t):
         #XXX: pynotify does not support gicon
-        n = pynotify.Notification('Device can be removed now', m.get_name(), _get_icon_name_from_gicon(m.get_icon()))
-        n.show()
+        Notify.init('indicator-usb')
+    	n = Notify.Notification.new('Device can be removed now', m.get_name(), _get_icon_name_from_gicon(m.get_icon()))
+    	n.show()
 
     def eject(self, s, m):
-        m.unmount(self._eject_cb)
+        m.unmount(Gio.MountUnmountFlags.NONE, None, self._eject_cb, None)
+
+
 
 class Main:
     mon = None
     ind = None
     menu = None
-    
+
     def __init__(self):
         #if running from source directory set a known icon
         if os.path.exists("icon.png"):
             icon = 'image-missing'
         else:
             icon = 'indicator-usb-panel'
-        self.ind = appindicator.Indicator('indicator-usb', icon, appindicator.CATEGORY_HARDWARE)
-        self.ind.set_status(appindicator.STATUS_PASSIVE)
+        self.ind = AppIndicator.Indicator.new('indicator-usb', icon, AppIndicator.IndicatorCategory.HARDWARE)
+        self.ind.set_status(AppIndicator.IndicatorStatus.PASSIVE)
 
         self.mon = Monitor(self)
 
@@ -82,7 +86,7 @@ class Main:
             self.mon.show_net = ss[1] == '1'
         except:
             pass
-        
+
         self.mon.init()
         self.save_config()
 
@@ -91,62 +95,61 @@ class Main:
         with open(os.path.expanduser('~/.config/indicator-usb'), 'w') as f:
             f.write('%i\n%i\n' % ((1 if self.mon.show_hdd else 0), (1 if self.mon.show_net else 0)))
         self.mon.refresh()    
-             
+
     def update(self):
         self.ind.set_status(
-            appindicator.STATUS_ACTIVE if len(self.mon.drives) > 0 else
-            appindicator.STATUS_PASSIVE)
-            
+            AppIndicator.IndicatorStatus.ACTIVE if len(self.mon.drives) > 0 else
+            AppIndicator.IndicatorStatus.PASSIVE)
         if self.menu is not None:
             self.menu.destroy()
-            
-        self.menu = gtk.Menu()
+
+        self.menu = Gtk.Menu()
 
         # Header
-        hdr = gtk.MenuItem('Safely remove devices')
+        hdr = Gtk.MenuItem(label='Safely remove devices')
         hdr.set_sensitive(False)
         hdr.show()
         self.menu.append(hdr)
-        
+
         # Separator
-        smi = gtk.SeparatorMenuItem()
+        smi = Gtk.SeparatorMenuItem()
         smi.show()
         self.menu.append(smi)
-        
+
         for i in self.mon.drives:
-            mi = gtk.ImageMenuItem(i.get_name())
+            mi = Gtk.ImageMenuItem(label=i.get_name())
             #XXX: gicon does not work...
-            mi.set_image(gtk.image_new_from_stock(_get_icon_name_from_gicon(i.get_icon()), gtk.ICON_SIZE_MENU))
+            mi.set_image(Gtk.Image.new_from_stock(_get_icon_name_from_gicon(i.get_icon()), Gtk.IconSize.MENU))
             self.menu.append(mi)
             mi.show()
             mi.connect('activate', self.mon.eject, i)
 
         # Separator
-        smi = gtk.SeparatorMenuItem()
+        smi = Gtk.SeparatorMenuItem()
         smi.show()
         self.menu.append(smi)
 
         # Options
-        mi = gtk.MenuItem('Devices')
+        mi = Gtk.MenuItem(label='Devices')
         mi.show()
         self.menu.append(mi)
 
-        om = gtk.Menu()
+        om = Gtk.Menu()
         om.show()
         mi.set_submenu(om)
-        
-        m = gtk.CheckMenuItem('Disks')
+
+        m = Gtk.CheckMenuItem(label='Disks')
         m.set_active(self.mon.show_hdd)
         m.connect('activate', self.on_option, 'hdd')
         m.show()
         om.append(m)
 
-        m = gtk.CheckMenuItem('Network folders')
+        m = Gtk.CheckMenuItem(label='Network folders')
         m.set_active(self.mon.show_net)
         m.connect('activate', self.on_option, 'net')
         m.show()
         om.append(m)
-        
+
         self.ind.set_menu(self.menu)
 
     def on_option(self, x, o):
@@ -155,8 +158,7 @@ class Main:
         if o == 'net':
             self.mon.show_net = x.get_active()
         self.save_config()
-        
-            
+
 m = Main()
-gtk.main()
-        
+Gtk.main()
+
